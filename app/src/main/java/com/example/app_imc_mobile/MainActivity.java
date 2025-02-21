e6b69254887b747f37f9a9bb701a1973e54a2f02
2830482311026 <2830482311026>
2025-02-20 22:10:26 -0300

    Atualizado

--- app/src/main/java/com/example/app_imc_mobile/MainActivity.java
+++ app/src/main/java/com/example/app_imc_mobile/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.app_imc_mobile;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +11,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btLimpar, btCalcular;
+    EditText editTextPeso, editTextAltura, viewTextValueIMC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
+        btCalcular = (Button) findViewById(R.id.btCalcular);
+        btLimpar = (Button) findViewById(R.id.btLimpar);
+        editTextPeso = (EditText) findViewById(R.id.editTextPeso);
+        editTextAltura = (EditText) findViewById(R.id.editTextAltura);
+
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -21,4 +31,5 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
 }
